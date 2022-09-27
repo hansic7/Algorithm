776f46f4ba1a51f65c7a824841561f8a38459e3f
hansic7 <109254058+hansic7@users.noreply.github.com>
2022-09-27 16:53:42 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if a>=90 and 100>=a:
+    print("A")
+elif a<=89 and a >= 80:
+    print("B")
+elif a<=79 and a >= 70:
+    print("C")
+elif a<=69 and a >= 60:
+    print("D")
+else:
+    print("F")
