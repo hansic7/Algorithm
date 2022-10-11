892666fe6388d11a0a1ced1186f679381548c91f
hansic7 <109254058+hansic7@users.noreply.github.com>
2022-10-11 15:53:50 +0900

    [Silver I] Title: 곱셈, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,10 +1,10 @@
-def dac(a, b, c):
+def ss(a,b,c):
     if b == 1:
-        return a % c
-    elif b % 2 == 0:
-        return (dac(a,b//2,c)**2)%c
+        return (a%c)
+    elif b % 2 == 1:
+        return (ss(a, b//2,c))**2*a%c
     else:
-        return ((dac(a,b//2,c)**2)*a)%c
-
-abc = list(map(int, input().split()))
-print(dac(abc[0], abc[1], abc[2]))
+        return (ss(a, b//2,c))**2%c
+        
+a,b,c = map(int,input().split())    
+print(ss(a,b,c))
