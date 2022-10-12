204b76837f00d75dc78966b0a6c1825495ad5202
hansic7 <109254058+hansic7@users.noreply.github.com>
2022-10-12 14:35:41 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
@@ -0,0 +1,36 @@
+n = int(input())
+
+def deep(n):
+    print('____'*n, end='')
+
+def pp(n):
+    deep(n)
+    print('"재귀함수가 뭔가요?"')
+
+def ss(n):
+    deep(n)   
+    print('"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.')
+    deep(n)
+    print('마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.')
+    deep(n)
+    print('그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."')
+
+def ans(n):
+    deep(n)
+    print('라고 답변하였지.')
+
+i = 0
+def recur(i):
+    pp(i)
+    if i == n:
+        deep(i)
+        print('"재귀함수는 자기 자신을 호출하는 함수라네"')
+        ans(i)
+        return
+    ss(i)
+    recur(i+1)
+    ans(i)
+
+
+print('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.')
+recur(i)
