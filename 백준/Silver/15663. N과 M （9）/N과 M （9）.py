7532942d27a2a1c3224707fcbbfbb63f9ffc84c2
hansic7 <109254058+hansic7@users.noreply.github.com>
2022-10-18 17:27:30 +0900

    [Silver II] Title: N과 M (9), Time: 120 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,20 +1,25 @@
-n, m = map(int, input().split())
-nums = sorted(list(map(int, input().split())))
+n,m = map(int,input().split())
+board = list(map(int, input().split()))
+# n, m = 4,2
+# board = [9,7,1,9]
+board.sort()
+N = len(board)
+s = []
+
 visited = [False] * n
-temp = []
 
 def dfs():
-    if len(temp) == m:
-        print(*temp)
+    if len(s) == m:
+        print(' '.join(map(str, s)))
         return
-    remember_me = 0
-    for i in range(n):
-        if not visited[i] and remember_me != nums[i]:
+    befo = 0
+    for i in range(N):
+        if board[i] != befo and not visited[i]:
+            befo = board[i]
             visited[i] = True
-            temp.append(nums[i])
-            remember_me = nums[i]
+            s.append(board[i])
             dfs()
+            s.pop()
             visited[i] = False
-            temp.pop()
 
 dfs()
