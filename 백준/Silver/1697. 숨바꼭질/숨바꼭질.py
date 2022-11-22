fc51217ecda578d30df3583bdaaf830baaf75e9e
hansic7 <109254058+hansic7@users.noreply.github.com>
2022-11-22 16:17:02 +0900

    [Silver I] Title: 숨바꼭질, Time: 152 ms, Memory: 34692 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -11,10 +11,10 @@ def bfs():
             print(dist[x])
             break
         for nx in (x-1, x+1, x*2):
-            if 0 <= nx <= MAX and not dist[nx]:
+            if 0 <= nx < MAX and not dist[nx]:
                 dist[nx] = dist[x] +1
                 q.append(nx)
 
-MAX = 10 ** 5 
-dist = [0] * (MAX + 1)
+MAX = 10 ** 5 + 1
+dist = [0] * MAX
 bfs()
