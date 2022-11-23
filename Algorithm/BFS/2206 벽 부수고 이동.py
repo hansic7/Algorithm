472d9b320357cb7693bbdd7611e8ffb4d5946300
hansic <hansic0124@gmail.com>
2022-11-23 14:51:01 +0900

    under repo name change

---
+++ Algorithm/BFS/2206 벽 부수고 이동.py
@@ -0,0 +1,12 @@
+from collections import deque
+import sys
+
+board = []
+N, M = map(int, input().split())
+for i in range(N):
+    board.append(list(map(int, sys.stdin.readline().split())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+s
