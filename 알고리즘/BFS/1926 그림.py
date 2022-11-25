a5a3d22d3910bcabd26d8eb1fcf7b057255b8ebe
hansic7 <hansic0124@gmail.com>
2022-11-25 15:25:34 +0900

    그림 수정

--- 알고리즘/BFS/1926 그림.py
+++ 알고리즘/BFS/1926 그림.py
@@ -1,10 +1,21 @@
 from collections import deque
 
-N, M = map(int, input().split())
+# N, M = map(int, input().split())
+N , M = 6,5
 
 board = []
-for _ in range(N):
-    board.append(list(map(int, input().split())))
+# for _ in range(N):
+#     board.append(list(map(int, input().split())))
+
+d = ['1 1 0 1 1',
+'0 1 1 0 0',
+'0 0 0 0 0',
+'1 0 1 1 1',
+'0 0 1 1 1',
+'0 0 1 1 1']
+
+for i in range(6):
+    board.append(list(map(int, d[i].split())))
 
 visited = []
 for i in range(N):
@@ -16,18 +27,22 @@ how_big = [0]
 
 def bfs(y,x):
     q = deque()
-    q.append(y,x)
-    
+    q.append([y,x])
+    visited[y][x] = 1
+    tmp = 1
+
     while q:
         y, x = q.popleft()
-        tmp = 1
+        
 
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
-            if 0<=ny<N and 0<=nx<M and not visited[ny][nx]:
+            if 0<=ny<N and 0<=nx<M and not visited[ny][nx] and board[ny][nx] == 1:
                 tmp += 1
                 q.append([ny,nx])
+                visited[ny][nx] = 1
+                print(f"this is tmp in bfs = {tmp}")
     
     how_big.append(tmp)
 
@@ -35,9 +50,10 @@ cnt = 0
             
 for j in range(N):
     for i in range(M):
-        if board[j][i] == 1:
+        if board[j][i] == 1 and not visited[j][i]:
             bfs(j,i)
             cnt += 1
+            
 
 print(cnt)
 print(max(how_big))
