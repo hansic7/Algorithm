ef0e2bab904a6882f17743d6b1c62c31be78b82b
hansic7 <109254058+hansic7@users.noreply.github.com>
2022-11-29 04:15:27 +0900

    [Gold V] Title: 상범 빌딩, Time: 260 ms, Memory: 122940 KB -BaekjoonHub

---
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.py
@@ -0,0 +1,52 @@
+from collections import deque
+
+while True:
+    L, C, R = map(int, input().split())
+    if L == 0 and C == 0 and R == 0:
+        exit()
+
+    board = []
+    for i in range(L):
+        tmp = []
+        for j in range(C):
+            tmp.append(list(input().strip()))
+        board.append(tmp)
+        input()
+
+    for i in range(L):
+        for j in range(C):
+            for k in range(R):
+                if board[i][j][k] == 'S':
+                    start = [i,j,k]
+                    board[i][j][k] = 0
+                elif board[i][j][k] == 'E':
+                    end = [i,j,k]
+
+    dz = [1,-1,0,0,0,0]                    
+    dx = [0,0,1,-1,0,0]
+    dy = [0,0,0,0,-1,1]             
+
+    def bfs():
+        q = deque()
+        q.append(start)
+        while q:
+            # print(f"this is que {q}")
+            z, y, x = q.popleft()
+            
+            
+            for i in range(6):
+                nz = z + dz[i]
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0<=nz<L and 0<=ny<C and 0<=nx<R:
+                    if board[nz][ny][nx] == "." or board[nz][ny][nx] == "E" :
+                        board[nz][ny][nx] = board[z][y][x] + 1
+                        q.append([nz,ny,nx])
+                        # print(f"{[nz,ny,nx]} and {board}")
+                        # print()
+                        if nz == end[0] and ny == end[1] and nx == end[2]:
+                            print(f"Escaped in {board[nz][ny][nx]} minute(s).")
+                            return
+        print("Trapped!")
+
+    bfs()
