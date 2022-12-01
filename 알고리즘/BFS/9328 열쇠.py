0dd3934be3d845ba2aed0a1c5654f7c557040e0f
hansic7 <hansic0124@gmail.com>
2022-12-01 22:30:12 +0900

    22.12.01

--- 알고리즘/BFS/9328 열쇠.py
+++ 알고리즘/BFS/9328 열쇠.py
@@ -12,6 +12,27 @@ N =1
 
 entr = []
 
+def bfs():
+    q = deque()
+    for i in entr:
+        q.append(i)
+        while q:
+            y,x = q.popleft()
+            for ny,nx in [(y+1, x), (y, x+1), (y-1, x), (y, x-1)]:
+                if 0<=ny<h and 0<=nx and not visited[ny][nx]:
+                    if map[ny][nx] == '.':
+                        q.append([ny, nx])
+                        visited[ny][nx] = True
+                    elif map[ny][nx] != '*':
+                        if map[ny][nx].islower():
+                            key.append(map[ny][nx])
+                            map[ny][nx] = '.'
+                            q.append([ny,nx])
+                        else:
+                            if map[ny][nx].islower() in key:
+                                q.append([ny,nx])
+                        visited[ny][nx] = True
+
 for _ in range(N):
     h , w = 5, 17
     # h, w = map(int, input().split())
@@ -31,4 +52,8 @@ for _ in range(N):
         elif map[i][w-1] != '*':
             entr.append([i,w-1])
 
-    
+    ##두번 들어갈거임 키만 찾을수 있어서
+    for _ in range(2):  
+        visited = [[0]*w for i in range(h)]
+        
+
