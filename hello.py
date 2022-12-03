4aa6df7879cb92990f7944ebcc704d6d9a13d27f
hansic7 <hansic0124@gmail.com>
2022-12-03 23:50:51 +0900

    22.12.03

--- hello.py
+++ hello.py
@@ -5,10 +5,10 @@ ply_str[2].append([3,2])
 ply_str[2].append([3,4])
 print(ply_str)
 
-for i,j in ply_str:
+for i,j in ply_str[z] for z in ply_str :
     print(i)
 
 # a = [2,3]
 # for i in a:
 #     a.append(7)
-#     print(a)
+    print(a)
