4c3ba3ddac15010d83dfa645d33aa81b45c7fae8
hansic7 <hansic0124@gmail.com>
2022-12-04 22:26:53 +0900

    22.12.04

--- 알고리즘/BFS/2146 다리만들기.py
+++ 알고리즘/BFS/2146 다리만들기.py
@@ -1,26 +1,41 @@
 from collections import deque
 
-N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
+dd = ['1 1 1 0 0 0 0 1 1 1',
+'1 1 1 1 0 0 0 0 1 1',
+'1 0 1 1 0 0 0 0 1 1',
+'0 0 1 1 1 0 0 0 0 1',
+'0 0 0 1 0 0 0 0 0 1',
+'0 0 0 0 0 0 0 0 0 1',
+'0 0 0 0 0 0 0 0 0 0',
+'0 0 0 0 1 1 0 0 0 0',
+'0 0 0 0 1 1 1 0 0 0',
+'0 0 0 0 0 0 0 0 0 0']
 
+# N = int(input())
+# board = [list(map(int, input().split())) for _ in range(N)]
 
+N = 10
+board= [list(map(int, dd[i].split())) for i in range(N)] ###
 
 
 visited = [[False]*N for _ in range(N)]
-def bfs(y,x):
+def bfs_edge(y,x):
     q = deque()
-    q.append(y,x)
-    edge = []
+    q.append([y,x])
+    edge = deque()
     while q:
         y, x = q.popleft()
         for ny,nx in [(y,x+1), (y,x-1), (y+1,x), (y-1, x)]:
             if 0<=ny<N and 0<=nx<N and not visited[ny][nx]:
-                if board[ny][nx] == 0:
-                    edge.append([ny,nx])
-                    visited[ny][nx] = True
-                    break
-                else:
+                if board[ny][nx] == 1:
                     q.append([ny,nx])
                     visited[ny][nx] = True
+                else:
+                    if [ny,nx] not in edge:
+                        edge.append([ny,nx])
     return(edge)
 
+for i in range(N):
+    for j in range(N):
+        if board[i][j] == 1:
+            
