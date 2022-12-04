c3b8d31be09fd115d4886666da43d61ee715a097
hansic7 <hansic0124@gmail.com>
2022-12-04 21:51:08 +0900

    22.12.04

--- 알고리즘/BFS/2146 다리만들기.py
+++ 알고리즘/BFS/2146 다리만들기.py
@@ -6,5 +6,21 @@ board = [list(map(int, input().split())) for _ in range(N)]
 
 
 
-def bfs():
-    for i in 
+visited = [[False]*N for _ in range(N)]
+def bfs(y,x):
+    q = deque()
+    q.append(y,x)
+    edge = []
+    while q:
+        y, x = q.popleft()
+        for ny,nx in [(y,x+1), (y,x-1), (y+1,x), (y-1, x)]:
+            if 0<=ny<N and 0<=nx<N and not visited[ny][nx]:
+                if board[ny][nx] == 0:
+                    edge.append([ny,nx])
+                    visited[ny][nx] = True
+                    break
+                else:
+                    q.append([ny,nx])
+                    visited[ny][nx] = True
+    return(edge)
+
