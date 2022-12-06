3016cc6829fe3fdb85a42afe09f22c33e5490085
hansic7 <hansic0124@gmail.com>
2022-12-06 17:39:24 +0900

    22.12.06

---
+++ 알고리즘/Dynamic Programming/1463 1로 만들기.py
@@ -0,0 +1,15 @@
+
+n = int(input())
+
+cnt = 0
+while n > 1:
+    print(f"n = {n} and cnt = {cnt} ")
+    if n % 3 == 0:
+        n = n // 3
+    elif n % 2 == 0:
+        n = n // 2
+    else:
+        n -= 1
+    cnt += 1
+print(cnt)
+    
