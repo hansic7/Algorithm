c7cf70dd61b8e990699c62902e34879950d0928c
hansic7 <109254058+hansic7@users.noreply.github.com>
2022-12-09 15:47:06 +0900

    [Silver III] Title: 피보나치 함수, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,37 +1,17 @@
 
 N = int(input())
 
-for i in range(N):
-    n = int(input())
-
-    cnt_0 = [0] * (n+1)
-    cnt_1 = [0] * (n+1)
-    cnt_0[0] = 1
-    if n != 0:
-        cnt_1[1] = 1
-
-    def fibo0(n):
-        global cnt_0
-        if n == 0:
-            return (1)
-        if n == 1:
-            return (0)
-        if cnt_0[n] != 0:
-            return (cnt_0[n])
-        cnt_0[n] = (fibo0(n-1) + fibo0(n-2))
-        return(cnt_0[n])
-
-    def fibo_1(n):
-        global cnt_1
-        if n == 0:
-            return (0)
-        if n == 1:
-            return (1)
-        if cnt_1[n] != 0:
-            return (cnt_1[n])
-        cnt_1[n] = fibo_1(n-1) + fibo_1(n-2)
-        return(cnt_1[n])
+def fibo(n):
+    zero = [1,0,1]
+    one = [0,1,1]
+    if n >= 3:
+      for i in range(3,n+1):
+        zero.append(zero[i-1] + zero[i-2])
+        one.append(one[i-1] + one[i-2])
     
-    fibo0(n)
-    fibo_1(n)
-    print(f"{cnt_0[n]} {cnt_1[n]}")
+    print(f"{zero[n]} {one[n]}")
+    
+
+for _ in range(N):
+    n = int(input())
+    fibo(n)
