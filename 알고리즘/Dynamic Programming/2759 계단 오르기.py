861ccf40417f53a119472f0c00c857f6b9324d0b
hansic7 <hansic0124@gmail.com>
2022-12-10 16:34:29 +0900

    22.12.10

--- 알고리즘/Dynamic Programming/2759 계단 오르기.py
+++ 알고리즘/Dynamic Programming/2759 계단 오르기.py
@@ -1,8 +1,31 @@
+ddd = [10,
+20,
+15,
+25,
+10,
+20
+]
 
-N = int(input())
+N = int(input()) ##
+# N = 6
 
-d = []
-[d.append(int(input())) for i in range(N)]
+s = []
+for i in range(N):
+    s.append(int(input()))
+# [s.append(int(ddd[i])) for i in range(N)] ##
 
-for i in range((N-1), 0, -1):
-    
+
+dp = []
+if N>=3:
+    dp.append(s[0])
+    dp.append(s[1] + s[0])
+    dp.append(max(s[2]+s[0], s[1]+s[2]))
+    for i in range(3,N):
+        dp.append(max(s[i]+s[i-1]+dp[i-3], s[i]+dp[i-2]))
+        print(dp)
+
+print(dp[(N-1)])
+if N == 1:
+    print(s[0])
+if N == 2:
+    print(s[1]+s[0])
