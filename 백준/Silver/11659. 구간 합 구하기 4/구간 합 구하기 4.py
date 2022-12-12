673a9a9641ae0ac44a42a1c66c749967d97f0038
hansic7 <109254058+hansic7@users.noreply.github.com>
2022-12-12 18:58:49 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 4976 ms, Memory: 128324 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+board = list(map(int, input().split()))
+sum_dp = [0]
+
+for i in range(N):
+    sum_dp.append(sum_dp[i]+board[i])
+
+for i in range(M):
+    a,b = map(int, input().split())
+    print(sum_dp[b] - sum_dp[a-1])
