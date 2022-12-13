9abbc7980049aa5da266c440ff9716742f956115
hansic7 <hansic0124@gmail.com>
2022-12-13 23:14:28 +0900

    2193 이친수

---
+++ 알고리즘/Dynamic Programming/2193 이친수.py
@@ -0,0 +1,11 @@
+dp = [[0,0] for i in range(10)]
+dp[0][0] = 1
+
+n = int(input())
+
+for i in range(1, n+1):
+    dp[i][0] = dp[i-1][1]
+    dp[i][1] = dp[i-1][1]
+    dp[i][1] = dp[i][1] + dp[i-1][0]
+
+print(sum(dp[n-1]))
