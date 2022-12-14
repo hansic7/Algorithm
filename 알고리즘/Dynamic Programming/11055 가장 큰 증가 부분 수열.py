e91febea9bf785be0a1b3a31c616497dcc82ecf8
hansic7 <hansic0124@gmail.com>
2022-12-14 16:15:56 +0900

    d

--- 알고리즘/Dynamic Programming/11055 가장 큰 증가 부분 수열.py
+++ 알고리즘/Dynamic Programming/11055 가장 큰 증가 부분 수열.py
@@ -3,26 +3,26 @@ flag = 0
 
 n = 10
 board = [1, 100, 2, 50, 60, 3, 5, 6, 7, 8]
+board = [3,9,4,9,6,8,7,2,9,8]
 
-n = int(input())
-board = list(map(int, input().split()))
+# n = int(input())
+# board = list(map(int, input().split()))
 print(board)
 dp = [0] * (n)
 dp[0] = board[0]
-print(dp)
-print()
+
 
 for i in range(1,n):
     if board[i-1] > board[i]:
-        j = i
-        while board[j-1] >= board[i] and j > 0:
+        j = i-1
+        while board[j] >= board[i] and j >= 1:
             j -= 1
-            # print(f"j = {j} i = {i}")
-        dp[i] = dp[j-1] + board[i]
+        if j == 0 and board[j] >= board[i]:
+            dp[i] = board[i]
+        else:
+            dp[i] = dp[j] + board[i]
     else:
         dp[i] = dp[i-1] + board[i]
-    print(dp)
-
-print()
+   
 print(dp)
 print(max(dp))
