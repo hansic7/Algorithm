875065bce36f535490b9418c37a635077d785e61
hansic7 <hansic0124@gmail.com>
2022-12-15 18:45:33 +0900

    15988 1,2,3 더하기 3

--- 알고리즘/Dynamic Programming/15988 1,2,3 더하기 3.py
+++ 알고리즘/Dynamic Programming/15988 1,2,3 더하기 3.py
@@ -1,10 +1,9 @@
 N = int(input())
-
-
+dp = [1,2,4]
+for i in range(3, 1000001):
+    dp.append((dp[i-3]%1000000009)+(dp[i-2]%1000000009)+(dp[i-1]%1000000009))
+    
 for _ in range(N):
     n = int(input())
-    dp = [1,2,4]
-    for i in range(3,n):
-        dp.append((dp[i-3]+dp[i-2]+dp[i-1])%1000000009)
         
-    print(dp[n-1])
+    print(dp[n-1]%1000000009)
