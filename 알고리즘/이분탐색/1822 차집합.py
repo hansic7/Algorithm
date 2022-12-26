7ace398b6df6b20b61fdb01ab31c6f664503ad34
hansic7 <hansic0124@gmail.com>
2022-12-26 22:23:37 +0900

    1822 차집합

---
+++ 알고리즘/이분탐색/1822 차집합.py
@@ -0,0 +1,30 @@
+n,m = 4, 3
+board =[2, 5, 11, 7]
+s = [9, 7, 4]
+s.sort()
+
+n,m = map(int,input().split())
+board = list(map(int, input().split()))
+s = list(map(int, input().split()))
+s.sort()
+
+result = []
+for i in board:
+    start, end = 0, len(s)-1
+    flag = 0
+    while start <= end:
+        mid = (start + end) // 2
+        if s[mid] == i:
+            flag = 1
+            break
+        if i < s[mid]:
+            end = mid -1
+        else:
+            start = mid + 1
+    if flag == 0:
+        result.append(i)
+
+result.sort()
+print(len(result))
+for i in result:
+    print(i, end= ' ')
