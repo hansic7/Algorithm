b9b7531526bdb0e020b93c6d161c54655a4c0c83
hansic7 <109254058+hansic7@users.noreply.github.com>
2023-01-02 17:02:59 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,15 +1,12 @@
-N, K = map(int, input().split())
-result = 0
-
+n,m = map(int, input().split())
 coins = []
-
-for i in range(N):
+for _ in range(n):
     coins.append(int(input()))
-    
-for i in range(N-1, -1, -1):
-    now = K // coins[i]
-    if now:
-        result += now
-        K -= now*coins[i]
 
+result = 0
+for i in range(n-1, -1, -1):
+    if (m // coins [i]) > 0:
+        
+        result = result + (m//coins[i])
+        m = m % coins[i]
 print(result)
