0d12eaa6ea01c970f58676b2fcbbb737a258007b
hansic7 <hansic0124@gmail.com>
2023-01-07 10:23:44 +0900

    구명보트

--- Programmers/Lv.2/구명보트.py
+++ Programmers/Lv.2/구명보트.py
@@ -21,6 +21,21 @@ people = [70, 80, 50]
 print(solution(people, 100))
 
 
+## 좀더 깔끔한 정답코드
+## 1명씩은 무조건 타야되는 상황에서 2명이 되는 상황만 뺀다
+## 위에 내 풀이는 살짝 때려맞춘느낌 'while a <= b' 부분이ㅇㅇ
+def solution(people, limit) :
+    answer = 0
+    people.sort()
+
+    a = 0
+    b = len(people) - 1
+    while a < b :
+        if people[b] + people[a] <= limit :
+            a += 1
+            answer += 1
+        b -= 1
+    return len(people) - answer
 
 
 
