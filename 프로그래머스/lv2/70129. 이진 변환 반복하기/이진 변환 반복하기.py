b6090123645a4d84dcd1bcc41b2992d02c41cf15
hansic7 <109254058+hansic7@users.noreply.github.com>
2023-06-28 21:08:31 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 8.30 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,14 +1,16 @@
 def solution(s):
-    answer = [0, 0]  # 이진 변환의 횟수와 제거된 0의 개수를 저장할 변수
-
-    while s != "1":  # s가 "1"이 될 때까지 반복
-        # 0 제거
-        removed_zeros = s.count("0")  # 제거된 0의 개수
-        s = s.replace("0", "")
-
-        # 이진 변환
-        s = bin(len(s))[2:]  # 길이를 2진법으로 변환한 문자열
-        answer[0] += 1  # 이진 변환 횟수 증가
-        answer[1] += removed_zeros  # 제거된 0의 개수 누적
-
+    answer = [0,0]
+    
+    while s != '1':
+        first_len = len(s)
+        tmp_cnt = 0
+        for i in s:
+            if i == '0': 
+                tmp_cnt += 1
+        answer[1] += tmp_cnt
+        answer[0] += 1
+        
+        last_len = first_len - tmp_cnt
+        s = bin(last_len)[2:]
+        
     return answer
