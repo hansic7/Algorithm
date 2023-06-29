90d4702cbd8a7589a303001b12793f29b385da72
hansic7 <109254058+hansic7@users.noreply.github.com>
2023-06-29 17:44:02 +0900

    [Silver I] Title: 카드 구매하기, Time: 220 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,11 @@
+n = int(input())
+p = [0] + list(map(int, input().split()))
+
+dp = [0] * (n + 2)
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + p[j])
+
+
+print(dp[n])
