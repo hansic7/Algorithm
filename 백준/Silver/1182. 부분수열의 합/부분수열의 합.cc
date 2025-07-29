ae32e6e70d3e291b1715338c013553ddc66f70fd
hansic7 <109254058+hansic7@users.noreply.github.com>
2025-07-29 23:11:33 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int N, sum;
+vector<int> arr;
+
+int cnt = 0;
+void solve(int idx, int tmp) {
+
+	if (idx == N) return;
+	if (tmp + arr[idx] == sum) cnt++;
+
+	solve(idx + 1, tmp);
+	solve(idx + 1, tmp + arr[idx]);
+}
+
+int main() {
+	
+	cin >> N >> sum;
+
+	for (int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		arr.push_back(tmp);
+	}
+
+	solve(0, 0);
+	cout << cnt;
+
+	return 0;
+}
