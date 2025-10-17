723cf94d44c1d086aac430127283b33f1ed173ae
hansic7 <109254058+hansic7@users.noreply.github.com>
2025-10-17 20:17:42 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34952 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,33 +1,32 @@
 from collections import deque
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+M, N = map(int, input().split())
 
-def bfs(y,x, desty, destx):
-    que = deque()
-    que.append([y,x])
-    graph[y][x] = 0
-    while que:
-        y, x = que.popleft()
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if -1 < nx < M and -1 < ny < N and graph[ny][nx] == 1:
-                if not visited[ny][nx]:
-                    que.append([ny,nx])
-                    visited[ny][nx] = True
-                    graph[ny][nx] = graph[y][x] + 1
-            if ny == desty and nx == destx:
-                return(graph[ny][nx]+1)
+dy = [0,-1,0,1]
+dx = [1,0,-1,0]
 
-N, M = map(int, input().split())
+arr = [] 
+for i in range(M):
+    arr.append(list(map(int, input().strip())))
 
-graph =[]
-destx, desty = M-1, N-1
+visited = [[0]*N for _ in range(M)] 
 
-visited = []
-for _ in range(N):
-    visited.append(M*[False])
-for _ in range(N):
-    graph.append(list(map(int, input())))
-print(bfs(0,0,desty,destx))
+q = deque()
+q.append([0,0])
+visited[0][0] = 0
+
+while q:
+    y,x = q.popleft()
+
+    if y == M-1 and x == N-1:
+        print(visited[y][x]+1)
+        exit()
+
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if  0 <= ny < M and 0 <= nx < N and arr[ny][nx] == 1 and not visited[ny][nx]:
+            q.append([ny,nx])
+            visited[ny][nx] = visited[y][x]+1
+    
+print(-1)
