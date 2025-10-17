a8ccb858c73a613edb3ad0812e8d788ea24a97d1
hansic7 <109254058+hansic7@users.noreply.github.com>
2025-10-17 17:54:57 +0900

    [Silver II] Title: 유기농 배추, Time: 224 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,37 +1,35 @@
 from collections import deque
 
-N = int(input())
+t = int(input())
 
-def bfs(y,x):
-    que = deque()
-    que.append([y,x])
-    graph[y][x] = 0
-    while que:
-        yy, xx = que.popleft()
-        for i in range(4):
-            ny = yy + dy[i]
-            nx = xx + dx[i]
-            if -1 < nx < n and -1 < ny < m and graph[ny][nx] == 1:
-                que.append([ny,nx])
-                graph[ny][nx] = 0
-
-for i in range(N):
-    n, m, how_many = map(int, input().split()) 
-    graph = []
-    for i in range(m):
-        graph.append(n * [0])
-
-    for i in range(how_many):
-        a, b = map(int, input().split())
-        graph[b][a] = 1
-
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
+for _ in range(t):
+    n, m, b = map(int, input().split())
     cnt = 0
-    for yy in range(m):
-        for xx in range(n):
-            if graph[yy][xx] == 1:
-                cnt += 1
-                bfs(yy,xx)
+    dy = [0, -1, 0, 1]
+    dx = [1, 0, -1, 0]
 
-    print (cnt)
+    q = deque()
+    visited = [[0]*n for _ in range(m)]
+    arr = [[0]*n for _ in range(m)]
+
+    for _ in range(b):
+        x, y = map(int, input().split())
+        arr[y][x] = 1
+
+    for i in range(m):
+        for j in range(n):
+            if arr[i][j] == 1 and not visited[i][j]:
+                q.append([i, j])
+                visited[i][j] = 1
+                while q:
+                    y, x = q.popleft()
+                    for dir in range(4):
+                        ny = y + dy[dir]
+                        nx = x + dx[dir]
+                        if 0 <= ny < m and 0 <= nx < n:
+                            if arr[ny][nx] and not visited[ny][nx]:
+                                q.append([ny, nx])
+                                visited[ny][nx] = 1
+                cnt += 1
+
+    print(cnt)
