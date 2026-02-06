9b5184fae324745f50f0f8708d777c5550e8a873
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-02-06 21:55:17 +0900

    [Silver II] Title: 종이의 개수, Time: 856 ms, Memory: 161060 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -1,35 +1,35 @@
+
+arr = []
+cnt = [0,0,0]
 N = int(input())
 
-board = []
 for _ in range(N):
-    board.append(list(map(int, input().split())))
+    arr.append(list(map(int, input().split())))
 
-minus_one = 0
-zero = 0
-one = 0
+# N = 9
+# arr = [[0, 0, 0, 1, 1, 1, -1, -1, -1], [0, 0, 0, 1, 1, 1, -1, -1, -1], [0, 0, 0, 1, 1, 1, -1, -1, -1], [1, 1, 1, 0, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 0, 0, 0], [0, 1, -1, 0, 1, -1, 0, 1, -1], [0, -1, 1, 0, 1, -1, 0, 1, -1], [0, 1, -1, 1, 0, -1, 0, 1, -1]]
 
-def dfs(y, x, n):
-    global minus_one, zero, one
-
-    num_check = board[y][x]
-    for i in range(y, y + n):
-        for j in range(x, x+n):
-            if num_check != board[i][j]:
-                #k, l 3인 이유는 9등분으로 쪼개야되니까 3*3칸 만드는 거임
-                for k in range(3):  
-                    for l in range(3):
-                        dfs(y+k*n//3, x+l*n//3 , n//3)
-                return
-    if num_check==0:
-        zero+=1
-    elif num_check==1:
-        one+=1
-    elif num_check==-1:
-        minus_one+=1
-    return
-
-dfs(0,0,N)
-
-print(minus_one)
-print(zero)
-print(one) 
+def dfs(n, y, x):
+    
+    global cnt
+    standard = arr[y][x]
+    tmp_cnt = [0,0,0]
+    volume = n //3
+    issame = True
+    for i in range(y, y+n):
+        if issame:
+            for j in range(x, x+n):
+                if standard != arr[i][j]:
+                    issame = False
+                    break
+    if issame:
+        cnt[standard] += 1
+    else:
+        for i in range(3):
+            for j in range(3):
+                dfs(volume, y + volume * i, x + volume *j)
+        
+dfs(N,0,0)
+print(cnt[-1])
+print(cnt[0])
+print(cnt[1])
