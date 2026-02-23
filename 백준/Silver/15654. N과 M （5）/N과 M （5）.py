c6700d053cfc4aafae371c6f88022eaed5ef816b
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-02-23 17:01:17 +0900

    [Silver III] Title: N과 M (5), Time: 172 ms, Memory: 112308 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,20 +1,26 @@
-n,m = map(int,input().split())
-board = list(map(int, input().split()))
-board.sort()
-N = len(board)
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+
 s = []
-
-
+arr.sort()
+visited = [False] * (N+1)
 
 def dfs():
-    if len(s) == m:
-        print(' '.join(map(str,s)))
+    global s, visited
+
+    if len(s) == M:
+        for st in s:
+            print(st, end = ' ')
+        print()
         return
-    for i in range(N):
-        if board[i] not in s:
-            s.append(board[i])
-            dfs()
-            s.pop()
     
+    for i in range(N):
+        if visited[i]:
+            continue
+        visited[i] = True
+        s.append(arr[i])
+        dfs()
+        s.pop()
+        visited[i] = False
 
 dfs()
