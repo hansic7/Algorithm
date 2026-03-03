ca438b1a8ad9cae14816febd8558d34803e97358
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-03-03 15:33:56 +0900

    Title: Date to Date, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260303/Date to Date/date-to-date.py
+++ 260303/Date to Date/date-to-date.py
@@ -5,12 +5,13 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 days1, days2 = 0,0
 
-for i in range(1, m1+1):
+for i in range(1, m1):
     days1 += num_of_days[i]
 days1 += d1
 
-for i in range(1, m2+1):
+for i in range(1, m2):
     days2 += num_of_days[i]
 days2 += d2
 
-print(days2 - days1)
+
+print(days2 - days1 + 1)
