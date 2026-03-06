2c47516c772f675d854cb1ef68794451b27b5883
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-03-06 15:05:40 +0900

    Title: 일렬로 서있는 소 2, Time: 56ms, Memory: 16MB, Status: Passed - Codetree

--- 260306/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 260306/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -6,10 +6,10 @@ def solve():
     for i in range(N):
         a = A[i]
         for j in range(i+1, N):
-            if a < A[j]:
+            if a <= A[j]:
                 b = A[j]
                 for z in range(j+1, N):
-                    if b < A[z]:
+                    if b <= A[z]:
                         cnt += 1 
 
     return cnt
