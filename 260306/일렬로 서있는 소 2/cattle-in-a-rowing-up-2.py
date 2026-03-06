c45e88a0de70679f3792a4307df3ce1c735f8e4e
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-03-06 15:04:07 +0900

    Title: 일렬로 서있는 소 2, Time: 57ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260306/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,18 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+def solve():
+    cnt = 0
+    for i in range(N):
+        a = A[i]
+        for j in range(i+1, N):
+            if a < A[j]:
+                b = A[j]
+                for z in range(j+1, N):
+                    if b < A[z]:
+                        cnt += 1 
+
+    return cnt
+
+print(solve())
+# Please write your code here.
