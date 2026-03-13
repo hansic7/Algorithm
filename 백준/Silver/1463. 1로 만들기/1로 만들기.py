578fb494b8f32f03c724fa1a6aa261ec7a39876a
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-03-13 15:26:09 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 118552 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,18 @@
 
 n = int(input())
 
-d = [0] * (n+1)
+dp = [-1] * (n+1)
+
+dp[1] = 0
 
 for i in range(2,n+1):
-    d[i] = d[i-1] +1
+    
+    tmp = dp[i-1] + 1
     if i % 2 == 0:
-        d[i] = min(d[i//2]+1, d[i])
+        tmp = min(tmp, dp[i//2]+1)
     if i % 3 == 0:
-        d[i] = min(d[i//3]+1, d[i])
+        tmp = min(tmp, dp[i//3]+1)
+    
+    dp[i] = tmp
 
-print(d[n])
+print(dp[n])
