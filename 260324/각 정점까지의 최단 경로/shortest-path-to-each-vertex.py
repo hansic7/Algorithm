d6636d3b21f8da8d2198484e59a9aa68247cf0d8
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-03-24 21:24:40 +0900

    Title: 각 정점까지의 최단 경로, Time: 2067ms, Memory: 44MB, Status: Time Limit Exceed - Codetree

---
+++ 260324/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -0,0 +1,40 @@
+import heapq
+
+n, m = map(int, input().split())
+k = int(input())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+
+distance = [10e8] * (n+1)
+
+def diijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist_in_q, now = heapq.heappop(q)
+        prev_dist = distance[now]
+        
+        if prev_dist < dist_in_q:
+            continue
+
+        for ed in edges:
+            if now == ed[0] or now == ed[1]:
+                if prev_dist + ed[1] < distance[ed[1]]:
+                    distance[ed[1]] = prev_dist + ed[2]
+                    heapq.heappush(q, (prev_dist + ed[2], ed[1]))
+
+                if prev_dist + ed[2] < distance[ed[0]]:
+                    distance[ed[0]] = prev_dist + ed[2]
+                    heapq.heappush(q, (prev_dist + ed[2], ed[0]))
+
+
+diijkstra(k)
+
+for i in range(1, n+1):
+    if distance[i] == 10e8:
+        print(-1)
+    else:
+        print(distance[i])
+
+# Please write your code here.
