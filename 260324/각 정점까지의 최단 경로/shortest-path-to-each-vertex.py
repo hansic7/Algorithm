c72f39cd7f8018ed2e265b6da721bb272c372d4e
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-03-24 22:08:46 +0900

    Title: 각 정점까지의 최단 경로, Time: 658ms, Memory: 54MB, Status: Passed - Codetree

--- 260324/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
+++ 260324/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -2,11 +2,18 @@ import heapq
 
 n, m = map(int, input().split())
 k = int(input())
-edges = [tuple(map(int, input().split())) for _ in range(m)]
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    x,y,z = map(int, input().split())
+    graph[x].append((y,z))
+    graph[y].append((x,z))
 
 distance = [10e8] * (n+1)
 
 def diijkstra(start):
+    global distance
     q = []
     heapq.heappush(q, (0, start))
     distance[start] = 0
@@ -17,16 +24,12 @@ def diijkstra(start):
         if distance[now] < dist_in_q:
             continue
 
-        for ed in edges:
-            if now == ed[0] or now == ed[1]:
-                if distance[now] + ed[1] < distance[ed[1]]:
-                    distance[ed[1]] = distance[now] + ed[2]
-                    heapq.heappush(q, (distance[now] + ed[2], ed[1]))
-                elif distance[now] + ed[2] < distance[ed[0]]:
-                    distance[ed[0]] = distance[now] + ed[2]
-                    heapq.heappush(q, (distance[now] + ed[2], ed[0]))
-
-
+        for gr in graph[now]:
+            new_dist = distance[now] + gr[1]
+            if new_dist < distance[gr[0]]:
+                distance[gr[0]] = new_dist
+                heapq.heappush(q, (new_dist, gr[0]))
+               
 diijkstra(k)
 
 for i in range(1, n+1):
