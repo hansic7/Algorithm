8814f7145e611244d38fb47030ac1c7e7782fe7b
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-04-01 17:00:16 +0900

    Title: 간선의 길이를 2배 늘리기, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 260401/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.py
+++ 260401/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.py
@@ -8,9 +8,11 @@ meta_graph = []
 
 for _ in range(m):
     u, v, w = map(int, input().split())
-    meta_graph.append((u,v))
+    
     graph[u].append([v,w])
     graph[v].append([u,w])
+
+    meta_graph.append((u,v,len(graph[u])-1,len(graph[v])-1))
 
 def diikstra(graph):
     pq = []
@@ -36,19 +38,20 @@ dist = diikstra(graph)
 result_before = dist[n]
 
 result_after = 0
-for u,v in meta_graph:
+for u,v,u_index,v_index in meta_graph:
     tmp_graph = copy.deepcopy(graph)
+    tmp_graph[u][u_index][1] *= 2
+    tmp_graph[v][v_index][1] *= 2
 
-    for i in range(len(tmp_graph[u])):
-        if tmp_graph[u][i][0] == v:
-            tmp_graph[u][i][1] *= 2
+    # for i in range(len(tmp_graph[u])):
+    #     if tmp_graph[u][i][0] == v:
+    #         tmp_graph[u][i][1] *= 2
+    #         break
+
+    # for i in range(len(tmp_graph[v])):
+    #     if tmp_graph[v][i][0] == u:
             
-            break
-
-    for i in range(len(tmp_graph[v])):
-        if tmp_graph[v][i][0] == u:
-            tmp_graph[v][i][1] *= 2
-            break
+    #         break
     
     tmp_dist = diikstra(tmp_graph)
     
