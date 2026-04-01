eb46bc93d9bf7ba8f596e2753742cb9fb5d14c2f
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-04-01 17:46:02 +0900

    Title: 간선의 길이를 2배 늘리기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260401/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.py
+++ 260401/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.py
@@ -12,13 +12,13 @@ for _ in range(m):
     graph[u].append([v,w])
     graph[v].append([u,w])
 
-    meta_graph.append((u,v,len(graph[u])-1,len(graph[v])-1))
+    meta_graph.append((u,v,w,len(graph[u])-1,len(graph[v])-1))
 
-def diikstra(graph):
+def diikstra(graph, start_index):
     pq = []
     dist = [10e8] * (n+1)
-    dist[1] = 0
-    heapq.heappush(pq, (0, 1))
+    dist[start_index] = 0
+    heapq.heappush(pq, (0, start_index))
 
     while pq:
         prev_dist, prev_index = heapq.heappop(pq)
@@ -34,23 +34,34 @@ def diikstra(graph):
     return dist
 
 
-dist = diikstra(graph)
-result_before = dist[n]
+dist = diikstra(graph, 1)
+dist_reverse = diikstra(graph, n)
 
+result_before = dist[n]
 result_after = 0
 
+# for i in range(1, n):
+#     tmp_graph = copy.deepcopy(graph)
 
-for i in range(1, n):
-    tmp_graph = copy.deepcopy(graph)
+#     for j in range(len(graph[i])):
+#         v, w = graph[i][j]
+#         if dist[i] + w == dist[v]:
+#             tmp_graph[i][j][1] *= 2
+#             break
 
-    for j in range(len(graph[i])):
-        v, w = graph[i][j]
-        if dist[i] + w == dist[v]:
-            tmp_graph[i][j][1] *= 2
-                    
-    tmp_dist = diikstra(tmp_graph)
+#     tmp_dist = diikstra(tmp_graph)
     
-    result_after = max(result_after, tmp_dist[n])
+#     result_after = max(result_after, tmp_dist[n])
+
+
+for u, v, w, u_index, v_index in meta_graph:
+    if dist[u] + w + dist_reverse[v] == result_before \
+    or dist[v] + w + dist_reverse[u] == result_before:
+        tmp_graph = copy.deepcopy(graph)
+        tmp_graph[u][u_index][1] *= 2
+        tmp_graph[v][v_index][1] *= 2
+        tmp_dist = diikstra(tmp_graph, 1)
+        result_after = max(result_after, tmp_dist[n])
 
 print(result_after - result_before)
 
