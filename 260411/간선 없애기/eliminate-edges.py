c6b7d3c5480c56df8fdfc30ec56fedae62400091
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-04-11 14:12:26 +0900

    Title: 간선 없애기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260411/간선 없애기/eliminate-edges.py
@@ -0,0 +1,44 @@
+import heapq
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    u,v,w = map(int, input().split())
+    graph[u].append([v,w])
+    graph[v].append([u,w])
+
+def diikstra():
+    dist = [10e8] * (n+1)
+    pq = []
+    heapq.heappush(pq, (0, 1))
+
+    while pq:
+        prev_dist, prev_index = heapq.heappop(pq)
+        for next_index, w in graph[prev_index]:
+            next_dist = prev_dist + w
+            if dist[next_index] > next_dist:
+                dist[next_index] = next_dist
+                heapq.heappush(pq, (next_dist, next_index))
+    
+    return dist
+
+dist = diikstra()
+result = 0
+
+for i in range(1, n+1):
+    for j in range(len(graph[i])):
+        v, w = graph[i][j]
+        if dist[v] == dist[i] + w:
+            graph[i][j] = [v, 10e8]
+            tmp_dist = diikstra()
+            graph[i][j] = [v, w]
+            if dist[n] != tmp_dist[n]:
+                result += 1
+
+print(result)
+
+
+
+# Please write your code here.
