b87db51f22131fd8fac5e6eafc8a7fe533740091
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-04-16 21:48:16 +0900

    Title: 이동 가능한 점들의 쌍, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260416/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
@@ -0,0 +1,39 @@
+n, m, P, Q = map(int, input().split())
+
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+queries = [tuple(map(int, input().split())) for _ in range(Q)]
+
+
+dist = [[10e8] * (n + 1) for _ in range(n+1)]
+red_dot = [[0] * (n+1) for _ in range(n+1)]
+
+
+for i in range(1, n+1):
+    dist[i][i] = 0
+    if i <= P:
+        for j in range(1, n+1):
+            red_dot[i][j] = 1
+
+for u,v,w in edges:
+    dist[u][v] = w
+
+
+for k in range(n+1):
+    for i in range(n+1):
+        for j in range(n+1):
+            if dist[i][j] > dist[i][k] + dist[k][j]:
+                dist[i][j] = dist[i][k] + dist[k][j]
+                red_dot[i][j] = max(red_dot[i][k], red_dot[k][j])
+
+possible_sum, dist_sum = 0,0
+for u,v in queries:
+    if red_dot[u][v]:
+        possible_sum += 1
+        dist_sum += dist[u][v]
+
+print(possible_sum)
+print(dist_sum)
+
+
+
+# Please write your code here.
