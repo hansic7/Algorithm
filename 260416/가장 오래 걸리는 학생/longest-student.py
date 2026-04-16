d9b7889604a0362c554447a0311126bfbeb87f54
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-04-16 16:15:18 +0900

    Title: 가장 오래 걸리는 학생, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 260416/가장 오래 걸리는 학생/longest-student.py
+++ 260416/가장 오래 걸리는 학생/longest-student.py
@@ -2,7 +2,7 @@ import heapq
 
 n, m = map(int, input().split())
 
-graph = [[] for _ in range(m)]
+graph = [[] for _ in range(n)]
 for _ in range(m):
     u,v,w = map(int, input().split())
     graph[u].append((v,w))
