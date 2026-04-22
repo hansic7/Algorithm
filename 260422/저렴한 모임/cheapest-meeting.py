3ea1ab6703a7c09f670c3b3bf7e1687a217a31b9
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-04-22 17:11:36 +0900

    Title: 저렴한 모임, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260422/저렴한 모임/cheapest-meeting.py
+++ 260422/저렴한 모임/cheapest-meeting.py
@@ -20,29 +20,26 @@ for k in range(1,n+1):
             if dist[i][j] > dist[i][k] + dist[k][j]:
                 dist[i][j] = dist[i][k] + dist[k][j]
 
-cost_sum = 0
-
 if dist[v1][e] == INT_MAX or dist[v2][e] == INT_MAX:
     print(-1)
     exit()
 
 taxi = -1
-taxi_s = 0
+taxi_k = 0
+ans = dist[v1][e] + dist[v2][e]
 for k in range(1,n+1):
     if dist[v1][k] + dist[k][e] == dist[v1][e] \
     and dist[v2][k] + dist[k][e] == dist[v2][e]:
-        if taxi < dist[k][e]:
-            taxi = dist[k][e]
-            taxi_s = k
+        ans = min(ans, dist[k][e] + dist[v1][k] + dist[v2][k])
 
-# print("k=", taxi_s)
+# print("k=", taxi_k)
 # print("taxi=", taxi)
-
-if taxi == -1:
-    print(dist[v1][e] + dist[v2][e])
-    exit()
-else:
-    print(dist[v1][taxi_s] + dist[v2][taxi_s] + taxi)
+print(ans)
+# if taxi == -1:
+#     print(dist[v1][e] + dist[v2][e])
+#     exit()
+# else:
+#     print(dist[v1][taxi_k] + dist[v2][taxi_k] + taxi)
 
 
 # Please write your code here.
