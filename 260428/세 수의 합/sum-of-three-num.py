f1334ecbe9f4b961ce37d4f85cbb129e1d886d41
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-04-28 18:44:11 +0900

    Title: 세 수의 합, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260428/세 수의 합/sum-of-three-num.py
+++ 260428/세 수의 합/sum-of-three-num.py
@@ -1,6 +1,20 @@
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
+dict = {}
+ans = 0
+
+for i in range(len(arr)):
+    for j in range(i+1, len(arr)):
+        elem = arr[i] + arr[j]
+        if elem in dict:
+            dict[elem] += 1
+        else:
+            dict[elem] = 1
 
 for elem in arr:
+    diff = k - elem
+    if diff in dict:
+        ans += dict[diff]
 
+print(ans//3)
 # Please write your code here.
