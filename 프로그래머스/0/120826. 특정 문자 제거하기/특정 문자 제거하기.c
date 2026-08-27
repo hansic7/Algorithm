c4087664c7f5b5faa2614dd70700a7b944356245
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-08-27 23:44:08 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 5.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* my_string, const char* letter) {
+    int len = 0;
+    while (my_string[len] != '\0')
+        len++;
+    
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc(len + 1);
+    
+    int j = 0;
+    for (int i = 0; i < len; i++) {
+        if (my_string[i] != letter[0]) {
+            answer[j] = my_string[i];
+            j++;
+        }
+    }
+    answer[j] = '\0';
+    return answer;
+}
