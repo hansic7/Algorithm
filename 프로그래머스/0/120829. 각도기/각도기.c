67a2e81468554681d0198520067b5424bf1d65ad
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-08-27 23:47:32 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int angle) {
+    int answer = 0;
+    if (angle < 90)
+        answer = 1;
+    else if (angle == 90)
+        answer = 2;
+    else if (angle < 180)
+        answer = 3;
+    else
+        answer = 4;
+    return answer;
+}
