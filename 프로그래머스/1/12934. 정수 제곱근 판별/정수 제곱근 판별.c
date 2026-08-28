e33e357e4a39a96daac439e455721636113543e7
hansic7 <109254058+hansic7@users.noreply.github.com>
2026-08-28 10:50:07 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 1.93 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(long long n) {
+    long long answer = -1;
+    
+    for (long long i = 1; i*i <= n; i++) {
+        if (i * i == n) {
+            answer = (i+1) * (i+1);
+            break;
+        }
+    }
+    
+    
+    return answer;
+}
